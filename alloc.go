18cee8cfee5348eaf59a22f7bc7ae4fcf5ca6e06
John <xiaoyang1258@yeah.net>
2021-06-14 21:48:45 +0000

    alloc: use sync.Pool

--- alloc.go
+++ alloc.go
@@ -2,8 +2,37 @@
 
 package trojan
 
-func malloc(n int) []byte {
-	return make([]byte, n)
+import (
+	"sync"
+	"unsafe"
+)
+
+var buffer = &sync.Pool{
+	New: func() interface{} {
+		b := make([]byte, 16*1024)
+		return &b[0]
+	},
 }
 
-func free(b []byte) {}
+func malloc(n int) []byte {
+	if n > 16*1024 {
+		return make([]byte, n)
+	}
+	type SliceHeader struct {
+		Data uintptr
+		Len  int
+		Cap  int
+	}
+	ptr := buffer.Get().(*byte)
+	return *(*[]byte)(unsafe.Pointer(&SliceHeader{
+		Data: uintptr(unsafe.Pointer(ptr)),
+		Len:  n,
+		Cap:  16 * 1024,
+	}))
+}
+
+func free(b []byte) {
+	if cap(b) == 16*1024 {
+		buffer.Put(&b[0])
+	}
+}
---
+++ alloc_test.go
@@ -0,0 +1,10 @@
+package trojan
+
+import "testing"
+
+func TestBuffer(t *testing.T) {
+	_, ok := buffer.Get().(*byte)
+	if !ok {
+		t.Fatal("buffer type error")
+	}
+}
